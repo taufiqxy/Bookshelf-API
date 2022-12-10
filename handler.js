78add3bd1333ae879911281dc7dfd30b20bcc5cb
taufiqx <taufiq.xy@gmail.com>
2022-12-10 16:30:25 +0700

    finished commit

--- handler.js
+++ handler.js
@@ -54,5 +54,130 @@ const add_book_handler = (request, h) => {
 }
 
 // handler show all books
+const get_all_books_handler = (request, h) => {
+    // create new books with only have id, name, and publisher
+    books_chope_properties = books.map(({id, name, publisher}) => {
+        let new_book = {id: id, name: name, publisher: publisher};
+        return new_book
+      });
+    
+    const response = h.response({
+        status: 'success',
+        data: {
+            books: books_chope_properties,
+        },
+    });
+    response.code(200);
+    return response;
+}
 
-module.exports = { add_book_handler };
+// get specific book handler
+const get_specific_book_handler = (request, h) => {
+    const { bookId } = request.params; // get params sent by client
+    const book = books.filter((b) => b.id === bookId)[0]; // check availabiltiy book
+
+    //if available
+    if (book !== undefined){
+        const response = h.response({
+            status: 'success',
+            data: {
+                books,
+            }
+        });
+        response.code(200);
+        return response;
+    }
+
+    // if not available
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku Tidak Ditemukan',
+    });
+    response.code(404);
+    return response;
+}
+
+// update book
+const update_book_handler = (request, h) => {
+    // load data from client
+    const {bookId} = request.params;
+    const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+
+    const updatedAt = new Date().toISOString();
+
+    // handle failed response
+    if(name === undefined){
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal memperbarui buku. Mohon isi nama buku',
+        });
+        response.code(400);
+        return response;
+    }
+    if(readPage > pageCount){
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+        });
+        response.code(400);
+        return response;
+    }
+    // check availability bookId
+    const index = books.findIndex((a_book) => a_book.id === bookId);
+    if(index === -1){
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal memperbarui buku. Id tidak ditemukan',
+        });
+        response.code(404);
+        return response;
+    }
+
+    // if success
+    if(index !== -1){
+        books[index] = {
+            ...books[index],
+            name, year, author, summary, publisher, pageCount, readPage, reading,
+            updatedAt,
+        };
+
+        const response = h.response({
+          status: 'success',
+          message: 'Buku berhasil diperbarui',
+        });
+        response.code(200);
+        return response;
+    }    
+}
+
+// Delete Books
+const delete_book_handler = (request, h) => {
+    const { bookId } = request.params;
+
+    const index = books.findIndex((a_book) => a_book.id === bookId);
+
+    if (index !== -1) {
+      books.splice(index, 1);
+      const response = h.response({
+        status: 'success',
+        message: 'Buku berhasil dihapus',
+      });
+      response.code(200);
+      return response;
+    }
+
+   const response = h.response({
+      status: 'fail',
+      message: 'Buku gagal dihapus. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+}
+
+module.exports = { 
+    add_book_handler,
+    get_all_books_handler,
+    get_specific_book_handler,
+    update_book_handler,
+    delete_book_handler,
+};
--- routes.js
+++ routes.js
@@ -1,4 +1,11 @@
-const { add_book_handler } = require('./handler');
+const { 
+    add_book_handler,
+    get_all_books_handler,
+    get_specific_book_handler,
+    update_book_handler,
+    delete_book_handler,
+}
+= require('./handler');
 
 // routes
 const routes = [
@@ -6,6 +13,26 @@ const routes = [
         method: 'POST',
         path: '/books',
         handler: add_book_handler,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: get_all_books_handler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{bookId}',
+        handler: get_specific_book_handler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{bookId}',
+        handler: update_book_handler,
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{bookId}',
+        handler: delete_book_handler,
     },
 
 ];
